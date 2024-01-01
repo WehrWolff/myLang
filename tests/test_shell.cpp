dacafa97dc50ed4b602f26ff12a217f76685446b
WehrWolff <135031808+WehrWolff@users.noreply.github.com>
2024-01-01 20:47:24 +0100

    Update Tests

--- tests/test_shell.cpp
+++ tests/test_shell.cpp
@@ -12,8 +12,8 @@ TEST(GrammarTest, AxiomAndRules) {
 }
 
 TEST(LRClosureTableTest, ClosureAndKernels) {
-    //Grammar grammar("A' -> A\nA -> a A\nA -> a");
-    LRClosureTable lrClosureTable(grammar);
+    Grammar grammar_("A' -> A\nA -> a A\nA -> a");
+    LRClosureTable lrClosureTable(grammar_);
     ASSERT_EQ(3, lrClosureTable.kernels.front().closure.size());
     ASSERT_EQ(4, lrClosureTable.kernels.size());
 } /*
